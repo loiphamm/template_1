5089c75977392d3964bf053ecf5a66a378e29ced
Loi Pham <loi.pham.work@gmail.com>
2026-09-23 13:40:47 +0700

    Tiệc Cưới Sẽ Tổ Chức

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -8,7 +8,7 @@ const sectionTitleStyle = {
   color: PRIMARY, fontFamily: '"Times New Roman", serif',
   fontSize: '20px', fontWeight: 700, letterSpacing: '1px',
 }
-const ADDRESS = 'Gia Đình Cô Dâu\nCây số 9, Xóm Re, Xã Đại Đồng, Tỉnh Phú Thọ'
+const ADDRESS = 'Tại Gia Đình Cô Dâu\nCây số 9, Xóm Re, Xã Đại Đồng, Tỉnh Phú Thọ'
 const MAPS_EMBED_URL = 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d4566.594752318561!2d105.5062256!3d20.425714499999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjDCsDI1JzMyLjYiTiAxMDXCsDMwJzIyLjQiRQ!5e1!3m2!1svi!2s!4v1790061307639!5m2!1svi!2s'
 
 export default function Location() {
@@ -17,7 +17,7 @@ export default function Location() {
       <img src={flowerImg} alt="" className="sec-flower fbg-lo11" />
 
       <Reveal as="h2" className="text-center uppercase" style={sectionTitleStyle}>
-        <AnimatedText text="Tiệc Cưới Sẽ Tổ Chức Tại" charDelay={38} from="sides" />
+        <AnimatedText text="Tiệc Cưới Sẽ Tổ Chức" charDelay={38} from="sides" />
       </Reveal>
 
       <Reveal className="mx-auto mt-3 max-w-sm md:max-w-[500px] text-center text-sm md:text-base tracking-wide whitespace-pre-line flex flex-col items-center pb-3"
